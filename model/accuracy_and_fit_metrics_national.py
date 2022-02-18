59a0b5ea6462ef27bd3f2c2bc9d373b7105ac64c
larriva <mlarriva@fcpdc.com>
2022-02-18 16:08:10 -0500

    writeup of data and improvement of metrics

--- model/accuracy_and_fit_metrics_national.py
+++ model/accuracy_and_fit_metrics_national.py
@@ -6,16 +6,25 @@ from sklearn.metrics import balanced_accuracy_score,accuracy_score,confusion_mat
 import pandas as pd
 import numpy as np
 
-cr = pd.read_csv(r'C:\Users\mlarriva\Documents\GitHub\inflation_cr_expansion\output\msa_df_cr.csv')
-flag = pd.read_csv(r'C:\Users\mlarriva\Documents\GitHub\inflation_cr_expansion\output\msa_df_flag.csv')
-cr = cr.melt(id_vars='year').dropna()
-flag = flag.melt(id_vars='year').dropna()
+# cr = pd.read_csv(r'C:\Users\mlarriva\Documents\GitHub\inflation_cr_expansion\output\msa_df_cr.csv')
+# flag = pd.read_csv(r'C:\Users\mlarriva\Documents\GitHub\inflation_cr_expansion\output\msa_df_flag.csv')
+# cr = cr.melt(id_vars='year').dropna()
+# flag = flag.melt(id_vars='year').dropna()
+# metrics = pd.DataFrame()
+# master = pd.merge(flag,cr, left_on=['year','variable'],right_on=['year','variable'])
+# master.columns=['year','variable','x','y']
+
+
+cr = pd.read_csv(r'C:\Users\matth\Documents\GitHub\inflation_cr_expansion\output\national_df_cr.csv')
+flag = pd.read_csv(r'C:\Users\matth\Documents\GitHub\inflation_cr_expansion\output\national_df_flag.csv')
 metrics = pd.DataFrame()
-master = pd.merge(flag,cr, left_on=['year','variable'],right_on=['year','variable'])
-master.columns=['year','variable','x','y']
+master = pd.merge(flag,cr, left_on=['date'],right_on=['date'])
+master.columns=['year','x','y']
+master['year'] = master['year'].apply(lambda x: int(x[0:4]))
+# master = master[master['year']>=2007]
 
 for i in range(0,100):
-	for i in range(2015,2020):
+	for i in range(2000,2020,4):
 		master_train = master[master['year']<i]
 		master_test = master[master['year']>=i]
 		X_train = master_train[['x']].astype(int).values
@@ -38,7 +47,6 @@ for i in range(0,100):
 
 
 		pred = np.matrix([int(x>0.5) for x in result.predict(X_test)]).T
-		bal = balanced_accuracy_score(y_test,pred)
 		cm = confusion_matrix(y_test, pred)
 		tn,fp,fn,tp = cm.ravel()
 		print(tn,fp,fn,tp)
@@ -55,8 +63,8 @@ for i in range(0,100):
 		var_vals.append(specificity)
 		var_vals.insert(0,i)
 
-		metrics = metrics.append(pd.DataFrame([var_vals],columns=['year']+list(cols)+['accuracy','precision','recall','f_score','specificity']))
-
+		metrics = metrics.append(pd.DataFrame([var_vals],columns=['year']+list(cols)+['accuracy','precision','recall','f_score','specificity']))		
+	
 	os = SMOTE()
 	x = master[['x']].astype(int).values
 	y = np.matrix(master['y'].astype(int).values).T
@@ -67,10 +75,11 @@ for i in range(0,100):
 	logit_model=sm.Logit(os_data_y,os_data_X)
 	# logit_model=sm.Logit(y,x)
 	result=logit_model.fit()
-
 	var_vals = result.summary2().tables[1]
 	var_vals = list(var_vals.values[0])
+
 	pred = np.matrix([int(x>=0.5) for x in result.predict(x)]).T
+	cm = confusion_matrix(y, pred)
 	accuracy = (tp+tn)/(tp+fp+fn+tn)
 	precision = tp/(tp+fp)
 	recall = tp/(tp+fn)
@@ -86,5 +95,8 @@ for i in range(0,100):
 	metrics = metrics.append(pd.DataFrame([var_vals],columns=['year']+list(cols)+['accuracy','precision','recall','f_score','specificity']))
 
 
+metrics.dropna(inplace=True)
 metrics = metrics.groupby('year').mean()
-metrics.to_csv(r'C:\Users\mlarriva\Documents\GitHub\inflation_cr_expansion\output\msa_level_accuracy_and_fit.csv')
+print(metrics)
+# metrics.to_csv(r'C:\Users\mlarriva\Documents\GitHub\inflation_cr_expansion\output\msa_level_accuracy_and_fit.csv')
+metrics.to_csv(r'C:\Users\matth\Documents\GitHub\inflation_cr_expansion\output\national_level_accuracy_and_fit.csv')
